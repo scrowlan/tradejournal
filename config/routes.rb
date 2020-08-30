71a8900388101c58309047fc36c7c6509e0d7d6c
Scott Rowland <scrowlan@gmail.com>
2020-08-30 16:15:30 -0500

    added devise and adjusted header/footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'users/new'
 
   root to: 'static_pages#home'
-
-  get '/signup', to: 'users#new'
 
   get '/help',    to: 'static_pages#help'
   get '/about',   to: 'static_pages#about'
